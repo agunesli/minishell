1c21c188e96bd9b32957e1b3acb846650a76c5b8
rooteleven <ammon.pro@gmail.com>
2022-07-21 00:11:59 +0200

    adding pwd

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -9,7 +9,7 @@ ft_pwd(char **cmd, t_data *my_data)
     if (cmd[1] != NULL)
     {
         perror("pwd: too many arguments");
-		return 0;
+		return -1;
     }
     else if (getcwd(buffer_pwd, sizeof(buffer_pwd)) != NULL)
 	{
@@ -18,7 +18,7 @@ ft_pwd(char **cmd, t_data *my_data)
 	else
 	{
 		perror("getcwd() error\n");
-		return 0;
+		return -1;
 	}
-	return 1;
+	return 0;
 }
