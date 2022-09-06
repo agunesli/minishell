c0d9a267979dc907224514f52b967b222a2b6518
Tanguy MATHER <tamather@student.42.fr>
2022-09-07 01:04:59 +0200

    norminette OKOK

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: tamather <tamather@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 20:45:27 by tamather          #+#    #+#             */
-/*   Updated: 2022/09/07 00:55:12 by tamather         ###   ########.fr       */
+/*   Updated: 2022/09/07 01:01:09 by tamather         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -165,7 +165,6 @@ int			len_split(char **bin);
 void		init_data(t_data *my_data, char *read, char **env);
 void		start_env(char **env, t_data *my_data);
 void		update_data(t_data *my_data);
-
 
 //free
 void		free_all(char **bin);
